f30a1577c8507bf8c2fb8d4655f8f413b48079f4
MichelLeblond <Michel_LeBlond@telus.net>
2021-09-28 15:50:31 -0700

    Upto and including 157

---
+++ Business/Repository/IRepository/IRoomOrderDetailsRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Repository.IRepository
+{
+    public interface IRoomOrderDetailsRepository
+    {
+        public Task<RoomOrderDetailsDTO> Create(RoomOrderDetailsDTO details);
+        public Task<RoomOrderDetailsDTO> MarkPaymentSuccessful(int Id);
+        public Task<RoomOrderDetailsDTO> GetRoomOrderDetail(int roomOrderId);
+        public Task<IEnumerable<RoomOrderDetailsDTO>> GetAllRoomOrderDetails();
+        public Task<bool> UpdateOrderStatus(int RoomOrderId, string Status);
+        public Task<bool> IsRoomBooked(int RoomId, DateTime checkInDate, DateTime checkOutDate);
+    }
+}
